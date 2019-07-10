b1c1cbb0968f48ff1af5ae73de5dfb8c2d619d0a
Francesca Paulin <fpaulin@solvedo.com>
2019-07-10 22:08:37 +0200

    notebook tutorial mobile app

--- predict_api.py
+++ predict_api.py
@@ -13,16 +13,18 @@ def hello_world():
 def add():
     req_data = request.get_json()
     bizprop = req_data['bizprop'] 
-    rooms = req_data['rooms'] = 1
-    age = req_data['age'] = 1
-    highways = req_data['highways'] =1
-    tax = req_data['tax'] = 1
-    ptratio = req_data['ptratio'] =1
-    lstat = req_data['lstat'] =1
+    rooms = req_data['rooms']
+    age = req_data['age'] 
+    highways = req_data['highways'] 
+    tax = req_data['tax']
+    ptratio = req_data['ptratio'] 
+    lstat = req_data['lstat']
     # This is where we load the actual saved model into new variable. 
     deep_and_wide_net = load_model('deep_and_wide_net.h5') 
     # Now we can use this to predict on new data 
     value = deep_and_wide_net.predict_on_batch(numpy.array([[bizprop, rooms, age, highways, tax, ptratio, lstat]], dtype=float)) 
     K.clear_session()
+    estimate =  "{:.0f}".format(value.item()*10000)
 
-    return str(value)
+
+    return str(estimate)
